d7acac9c1cae62a4bbd0e6eedffa9cf7ce0c3493
DraCaligari <tatianagarcia0695@hotmail.com>
2025-04-21 12:58:23 -0600

    Comentar el código relacionado con la comparación de productos financieros y la salida de factura

--- ActividadTipoDatos/Program.cs
+++ ActividadTipoDatos/Program.cs
@@ -143,33 +143,63 @@
 // Console.WriteLine($"           Tax: {taxPercentage:P2}");
 // Console.WriteLine($"     Total Billed: {total:C}");
 
-string customerName = "Ms. Barros";
+// string customerName = "Ms. Barros";
 
-string currentProduct = "Magic Yield";
-int currentShares = 2975000;
-decimal currentReturn = 0.1275m;
-decimal currentProfit = 55000000.0m;
+// string currentProduct = "Magic Yield";
+// int currentShares = 2975000;
+// decimal currentReturn = 0.1275m;
+// decimal currentProfit = 55000000.0m;
 
-string newProduct = "Glorious Future";
-decimal newReturn = 0.13125m;
-decimal newProfit = 63000000.0m;
+// string newProduct = "Glorious Future";
+// decimal newReturn = 0.13125m;
+// decimal newProfit = 63000000.0m;
 
-Console.WriteLine($"Dear {customerName},");
-Console.WriteLine($"As a customer of our {currentProduct} offering we are excited to tell you about a new financial product that would dramatically increase your return.\n");
-Console.WriteLine($"Currently, you own {currentShares:N} shares at a return of {currentReturn:P}.\n");
-Console.WriteLine($"Our new product, {newProduct} offers a return of {newReturn:P}.  Given your current volume, your potential profit would be {newProfit:C}.\n");
+// Console.WriteLine($"Dear {customerName},");
+// Console.WriteLine($"As a customer of our {currentProduct} offering we are excited to tell you about a new financial product that would dramatically increase your return.\n");
+// Console.WriteLine($"Currently, you own {currentShares:N} shares at a return of {currentReturn:P}.\n");
+// Console.WriteLine($"Our new product, {newProduct} offers a return of {newReturn:P}.  Given your current volume, your potential profit would be {newProfit:C}.\n");
 
-Console.WriteLine("Here's a quick comparison:\n");
+// Console.WriteLine("Here's a quick comparison:\n");
 
-string comparisonMessage = "";
+// string comparisonMessage = "";
 
-comparisonMessage = currentProduct.PadRight(20);
-comparisonMessage += String.Format("{0:P}", currentReturn).PadRight(10);
-comparisonMessage += String.Format("{0:C}", currentProfit).PadRight(20);
+// comparisonMessage = currentProduct.PadRight(20);
+// comparisonMessage += String.Format("{0:P}", currentReturn).PadRight(10);
+// comparisonMessage += String.Format("{0:C}", currentProfit).PadRight(20);
 
-comparisonMessage += "\n";
-comparisonMessage += newProduct.PadRight(20);
-comparisonMessage += String.Format("{0:P}", newReturn).PadRight(10);
-comparisonMessage += String.Format("{0:C}", newProfit).PadRight(20);
+// comparisonMessage += "\n";
+// comparisonMessage += newProduct.PadRight(20);
+// comparisonMessage += String.Format("{0:P}", newReturn).PadRight(10);
+// comparisonMessage += String.Format("{0:C}", newProfit).PadRight(20);
 
-Console.WriteLine(comparisonMessage);
+// Console.WriteLine(comparisonMessage);
+
+// const string input = "<div><h2>Widgets &trade;</h2><span>5000</span></div>";
+
+// string quantity = "";
+// string output = "";
+
+// const string openSpan = "<span>";
+// const string closeSpan = "</span>";
+
+// int quantityStart = input.IndexOf(openSpan) + openSpan.Length; 
+// int quantityEnd= input.IndexOf(closeSpan);
+// int quantityLength = quantityEnd - quantityStart;
+// quantity = input.Substring(quantityStart, quantityLength);
+// quantity = $"Quantity: {quantity}";
+
+// const string tradeSymbol = "&trade;";
+// const string regSymbol = "&reg;";
+// output = input.Replace(tradeSymbol, regSymbol);
+
+// const string openDiv = "<div>";
+// int divStart = output.IndexOf(openDiv);
+// output = output.Remove(divStart, openDiv.Length);
+
+// const string closeDiv = "</div>";
+// int divCloseStart = output.IndexOf(closeDiv);
+// output = "Output: " + output.Remove(divCloseStart, closeDiv.Length);
+
+// Console.WriteLine(quantity);
+// Console.WriteLine(output);
+
