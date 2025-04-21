a701f21a3ce8a1f4864fe64db92c8a7e2632d94e
DraCaligari <tatianagarcia0695@hotmail.com>
2025-04-21 12:48:27 -0600

    Comentar código de operaciones de división y agregar salida de factura

--- ActividadTipoDatos/Program.cs
+++ ActividadTipoDatos/Program.cs
@@ -49,18 +49,96 @@
 // Console.WriteLine($"Total: {total}");
 // Console.WriteLine($"Invalid values: {message}");
 
-int value1 = 11; 
-decimal value2 = 6.2m;
-float value3 = 4.3f;
+// int value1 = 11; 
+// decimal value2 = 6.2m;
+// float value3 = 4.3f;
 
-int result1 = (int)Math.Round(value1 / value2);
+// int result1 = (int)Math.Round(value1 / value2);
 
-decimal result2 = value2 / (decimal)value3;
+// decimal result2 = value2 / (decimal)value3;
 
-float result3 = value3 / value1;
+// float result3 = value3 / value1;
 
-Console.WriteLine($"Divide value1 by value2: {result1}");
+// Console.WriteLine($"Divide value1 by value2: {result1}");
 
-Console.WriteLine($"Divide value2 by value3: {result2}");
+// Console.WriteLine($"Divide value2 by value3: {result2}");
 
-Console.WriteLine($"Divide value3 by value1: {result3}");
+// Console.WriteLine($"Divide value3 by value1: {result3}");
+
+// string[] pallets = [ "B14", "A11", "B12", "A13" ];
+
+// Console.WriteLine("Sorted...");
+// Array.Sort(pallets);
+// foreach (var pallet in pallets)
+// {
+//     Console.WriteLine($"-- {pallet}");
+// }
+
+// string[] pallets = [ "B14", "A11", "B12", "A13" ];
+
+// Console.WriteLine("Sorted...");
+// Array.Sort(pallets);
+// foreach (var pallet in pallets)
+// {
+//     Console.WriteLine($"-- {pallet}");
+// }
+
+// Console.WriteLine("");
+// Console.WriteLine("Reversed...");
+// Array.Reverse(pallets);
+// foreach (var pallet in pallets)
+// {
+//     Console.WriteLine($"-- {pallet}");
+// }
+
+// string[] pallets = [ "B14", "A11", "B12", "A13" ];
+// Console.WriteLine("");
+
+// Array.Clear(pallets, 0, 2);
+// Console.WriteLine($"Clearing 2 ... count: {pallets.Length}");
+// foreach (var pallet in pallets)
+// {
+//     Console.WriteLine($"-- {pallet}");
+// }
+
+// string pangram = "The quick brown fox jumps over the lazy dog";
+// string[] words = pangram.Split(' ');
+
+// for (int i = 0; i < words.Length; i++)
+// {
+//     char[] letters = words[i].ToCharArray();
+//     Array.Reverse(letters);
+//     words[i] = new string(letters);
+// }
+
+// string result = string.Join(" ", words);
+// Console.WriteLine(result);
+
+// string orderStream = "B123,C234,A345,C15,B177,G3003,C235,B179";
+// string[] orders = orderStream.Split(',');
+// Array.Sort(orders);
+
+// foreach (string orderId in orders)
+// {
+//     if (orderId.Length != 4)
+//     {
+//         Console.WriteLine($"{orderId}\t- Error");    
+//     }
+//     else 
+//     {
+//         Console.WriteLine(orderId); 
+//     }
+// }
+
+
+int invoiceNumber = 1201;
+decimal productShares = 25.4568m;
+decimal subtotal = 2750.00m;
+decimal taxPercentage = .15825m;
+decimal total = 3185.19m;
+
+Console.WriteLine($"Invoice Number: {invoiceNumber}");
+Console.WriteLine($"   Shares: {productShares:N3} Product");
+Console.WriteLine($"     Sub Total: {subtotal:C}");
+Console.WriteLine($"           Tax: {taxPercentage:P2}");
+Console.WriteLine($"     Total Billed: {total:C}");
